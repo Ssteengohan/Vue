da14889ace19c4773d469ddb758c5c83e4164c62
FIRST_GIO LAST-TODOREVIC <EMAIL_2048757@TALNET.NL>
2022-12-25 20:08:06 +0100

    Ep18 Two Mental Leaps to Script Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
+
+// eslint-disable-next-line no-undef
+let message = $ref("Hello world");
+
+let doSomething = () => {
+  alert("doing it now");
+};
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <button @click="doSomething">CLick me</button>
   </main>
 </template>
